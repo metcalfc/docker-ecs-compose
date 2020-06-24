b63e9c23261f8e3dfedb9e145adffb24ca16a8b8
Chad Metcalf <metcalfc@gmail.com>
2020-06-24 11:36:55 -0700

    Fixing whitespace.

--- app/app.py
+++ app/app.py
@@ -7,10 +7,13 @@ from datetime import datetime
 app = Flask(__name__)
 redis = StrictRedis(host='backend', port=6379)
 
+
 @app.route('/')
 def home():
     redis.lpush('times', datetime.now().strftime('%Y-%m-%dT%H:%M:%S%z'))
-    return render_template('index.html', title='Home', times=redis.lrange('times', 0, -1))
+    return render_template('index.html', title='Home',
+                           times=redis.lrange('times', 0, -1))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
